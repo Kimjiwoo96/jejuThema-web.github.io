43c6c9664dee6fbc1e23e5b0e88f74101b2b23b9
Kimjiwoo96 <117326598+Kimjiwoo96@users.noreply.github.com>
2023-11-15 14:40:02 +0900

    투어리즘 추가

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// #Tourism swiper
+var swiper = new Swiper("#Tourism .mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: "#Tourism .swiper-pagination",
+      clickable: true,
+    },
+  });
